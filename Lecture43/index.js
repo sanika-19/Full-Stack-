cd42e48e8e5331ef959bab7dd42e418e2ed657c0
sanika-19 <moresanika1925@gmail.com>
2026-03-04 12:59:37 +0530

    Functions in js

---
+++ Lecture43/index.js
@@ -0,0 +1,52 @@
+// // Plane and arrow Functions
+
+// function print(nm){
+//     console.log(nm);
+// }
+
+// print("sanika")
+// function say(){
+//     console.log("Hello!!");
+// }
+// // function call
+// say();
+// function printcnt(){
+//     for(let i=1;i<=100;i++){
+//         console.log(i)
+//     }
+// }
+
+// printcnt();
+// function getavg(num1,num2){
+//     let avg=(num1+num2)/2;
+//     console.log(avg);
+// }
+// getavg(3,70);
+
+// // Returning the value from the function
+// function getsum(a,b,c){
+//     let sum = a+b+c;
+//    return sum;
+// }
+
+// let val=getsum(1,2,3);
+// console.log("Sum="+val);
+
+// let solve= function(a,b){
+//   return a*b;
+// }
+// console.log(solve(2,30));
+
+// ans=function(num){
+//     return num**2;
+// }
+// console.log(ans(5));
+
+
+
+// Arrow function
+let getExp=(a,b)=>{
+    return a**b;
+}
+
+console.log(getExp(2,3));
