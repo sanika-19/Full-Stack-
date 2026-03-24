520fec386ef5aa1cc15a5858f6e752caefc5328c
sanika-19 <moresanika1925@gmail.com>
2026-03-24 13:38:52 +0530

    add Temporal Zone

---
+++ Lecture46/index.js
@@ -0,0 +1,44 @@
+
+
+var age=15;
+
+// console.log(age);
+
+
+// {
+//     console.log(age);
+// }
+// if(true){
+//     console.log(age);
+// }
+// function sayhell(){
+//   console.log(age);
+// }
+// sayhell();
+ 
+
+// Temporal Dead Zone
+console.log(marks);
+console.log("Sanika");
+comnsole.log("More");
+let marks=100;
+console.log(marks);
+
+
+// function sayhii(){
+//     var nm="Earth";
+//     console.log("Hello, " + nm);
+// }
+// sayhii();
+
+
+// {
+//     var city="Pune";
+// }
+// console.log(city);
+
+
+
+
+
+
